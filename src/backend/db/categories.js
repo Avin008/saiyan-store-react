0689f81ab0e18ed309b39b23accf947bb97df77e
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-04-07 17:33:21 +0530

    feat: added category data

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -8,20 +8,35 @@ import { v4 as uuid } from "uuid";
 export const categories = [
   {
     _id: uuid(),
-    categoryName: "fiction",
-    description:
-      "literature in the form of prose, especially novels, that describes imaginary events and people",
+    categoryName: "tshirts",
+    name: "Dragon Ball Z",
+    img: "https://saiyanshop.netlify.app/Assets/img/img1.webp",
   },
   {
     _id: uuid(),
-    categoryName: "non-fiction",
-    description:
-      "Non-fiction is writing that gives information or describes real events, rather than telling a story.",
+    categoryName: "tshirts",
+    name: "My Hero Academia",
+    img: "https://saiyanshop.netlify.app/Assets/img/img2.jpg",
   },
   {
     _id: uuid(),
-    categoryName: "horror",
-    description:
-      "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
+    categoryName: "tshirts",
+    name: "Dr. Stone",
+    img: "https://saiyanshop.netlify.app/Assets/img/img3.webp",
+  },
+  {
+    categoryName: "tshirts",
+    name: "Haikyuu",
+    img: "https://saiyanshop.netlify.app/Assets/img/img4.webp",
+  },
+  {
+    categoryName: "tshirts",
+    name: "Death Note",
+    img: "https://saiyanshop.netlify.app/Assets/img/img5.webp",
+  },
+  {
+    categoryName: "tshirts",
+    name: "Attack On Titan",
+    img: "https://saiyanshop.netlify.app/Assets/img/img6.jpg",
   },
 ];
