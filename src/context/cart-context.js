db32a28d2151b6ecd1f18bc74aa6175ea61fe6a4
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-11-11 21:48:11 +0530

    added cart data api call

--- src/context/cart-context.js
+++ src/context/cart-context.js
@@ -1,9 +1,25 @@
-import { createContext, useContext, useState } from "react";
-
+import axios from "axios";
+import { createContext, useContext, useEffect, useState } from "react";
+import { useAuthContext } from "../context/auth-context";
 const CartContext = createContext();
 
 const CartContextProvider = ({ children }) => {
   const [cartState, setCartState] = useState([]);
+  const { authState } = useAuthContext();
+
+  useEffect(() => {
+    const getCartData = async () => {
+      const res = await axios.get("/api/user/cart", {
+        headers: {
+          authorization: authState.token,
+        },
+      });
+
+      setCartState(res.data.cart);
+    };
+
+    getCartData();
+  }, [authState.status]);
 
   return (
     <CartContext.Provider value={{ cartState, setCartState }}>
