ce8b137ab09febdad34ec7229b15b8a14593b2b4
Avinash Mahananda <32406268+Avin008@users.noreply.github.com>
2022-11-11 17:27:24 +0530

    added input && submit handler

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,41 +1,77 @@
 import { Link } from "react-router-dom";
 import "./signup.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Signup = () => {
   useEffect(() => {
     document.title = "Signup | Saiyan Store";
   }, []);
 
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+
+  const [signUpData, setSignUpData] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: "",
+  });
+
+  const inputHandler = (e) => {
+    const { name, value } = e.target;
+    setSignUpData((prev) => ({ ...prev, [name]: value }));
+  };
+
   return (
     <div className="form-container">
-      <form className="login-form">
+      <form className="login-form" onSubmit={submitHandler}>
         <h1 className="login-heading">Sign Up</h1>
 
         <div className="input-group">
           <label className="input-label" htmlFor="Firstname">
             First name
           </label>
-          <input type="text" placeholder="john" />
+          <input
+            type="text"
+            placeholder="john"
+            name="firstname"
+            onChange={inputHandler}
+          />
         </div>
         <div className="input-group">
           <label className="input-label" htmlFor="lastname">
             Last name
           </label>
-          <input type="password" placeholder="*************" />
+          <input
+            type="text"
+            placeholder="lastname"
+            name="lastname"
+            onChange={inputHandler}
+          />
         </div>
 
         <div className="input-group">
           <label className="input-label" htmlFor="email">
             Email
           </label>
-          <input type="email" placeholder="johndoe@gmail.com" />
+          <input
+            type="email"
+            name="email"
+            placeholder="johndoe@gmail.com"
+            onChange={inputHandler}
+          />
         </div>
         <div className="input-group">
           <label className="input-label" htmlFor="email">
             Password
           </label>
-          <input type="password" placeholder="*************" />
+          <input
+            type="password"
+            name="password"
+            placeholder="*************"
+            onChange={inputHandler}
+          />
         </div>
         <div className="btn-container">
           <button className="primary-btn">Sign Up</button>
